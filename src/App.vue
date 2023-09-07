6c93f438030d91d2b3b9d6904ce027eea81085fe
duzhaoquan <zhaoquan.du@mxplayer.in>
2023-09-07 20:33:32 +0800

    feat: tabs1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TabsViews from './views/tabs/TabsViews.vue'
+// import TabsViews from './views/tabs/TabsViews.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <TabsViews></TabsViews>
+  <!-- <TabsViews></TabsViews> -->
+  <RouterView />
 </template>
 
 <style scoped></style>
--- src/views/tabs/TabsViews.vue
+++ src/views/tabs/TabsViews.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { Tabbar, TabbarItem } from 'vant'
 import { ref } from 'vue'
-
+import { RouterView } from 'vue-router'
 const active = ref('')
 </script>
 
 <template>
+  <RouterView />
   <Tabbar v-model="active">
     <TabbarItem name="home" icon="home-o">首页</TabbarItem>
     <TabbarItem name="order" icon="bars">订单</TabbarItem>
