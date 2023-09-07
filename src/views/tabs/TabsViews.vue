b4d617344d412b3cd3835e0f8212a036e8e6bd0b
duzhaoquan <zhaoquan.du@mxplayer.in>
2023-09-07 14:48:36 +0800

    feat: tabs

---
+++ src/views/tabs/TabsViews.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Tabbar, TabbarItem } from 'vant'
+import { ref } from 'vue'
+
+const active = ref('')
+</script>
+
+<template>
+  <Tabbar v-model="active">
+    <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+    <TabbarItem name="order" icon="bars">订单</TabbarItem>
+    <TabbarItem name="me" icon="contact">我的</TabbarItem>
+  </Tabbar>
+</template>
