5ee079f533d584d026f59b9d81cf633cb1e28386
duzhaoquan <zhaoquan.du@mxplayer.in>
2023-09-20 16:48:18 +0800

    feat: home top

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,15 @@ fetchTest()
   <RouterView />
 </template>
 
-<style scoped></style>
+<style>
+@import 'normalize.css';
+
+#app {
+  /** 字体抗锯齿，让字体看起来更清晰 */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  font-size: 14px;
+  padding: 0;
+}
+</style>
--- src/views/tabs/home/components/TheTop.vue
+++ src/views/tabs/home/components/TheTop.vue
@@ -15,7 +15,15 @@ defineProps<IProps>()
       <img class="shopcart-icon" src="@/assets/imgs/index_page/shopcart.png" />
       <img class="comments-icon" src="@/assets/imgs/index_page/comments.png" />
     </div>
-    <VanSearch shape="round"> </VanSearch>
+    <VanSearch
+      shape="round"
+      background="linear-gradient(to right, rgb(53, 200, 250), rgb(31, 175, 243))"
+      placeholder="布鲁斯披萨"
+    >
+      <template #right-icon>
+        <div>搜索</div>
+      </template>
+    </VanSearch>
     <div class="search-recomend">
       <div v-for="v in recomments" :key="v.value" class="tag">{{ v.label }}</div>
     </div>
@@ -24,7 +32,7 @@ defineProps<IProps>()
 
 <style lang="scss">
 .home-top {
-  background: liner-gradient(to right, rgb(53, 200, 250), rgb(31, 175, 243));
+  background: linear-gradient(to right, rgb(53, 200, 250), rgb(31, 175, 243));
   color: white;
   .top {
     display: flex;
@@ -33,6 +41,40 @@ defineProps<IProps>()
     line-height: 15px;
     font-size: 15px;
     font-weight: bold;
+    .location-icon {
+      width: 20px;
+      height: 20px;
+    }
+    .location {
+      flex: 1;
+    }
+    .shopcart-icon {
+      width: 24px;
+      height: 24px;
+    }
+    .comments-icon {
+      width: 28px;
+      height: 24px;
+      margin-left: 10px;
+    }
+  }
+  .search-recomend {
+    display: flex;
+    padding: 0 10px 8px;
+    .tag {
+      font-size: 12px;
+      border-radius: 10px;
+      background: rgb(242, 242, 242, 0.3);
+      padding: 2px 8px;
+      margin-right: 10px;
+    }
+  }
+}
+</style>
+<style lang="scss">
+.home-top {
+  .van-field__right-icon {
+    margin-right: 0;
   }
 }
 </style>
