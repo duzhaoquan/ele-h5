2d60e60d8874e11da2b59416572e17645a2e5780
duzhaoquan <zhaoquan.du@mxplayer.in>
2023-11-03 11:26:59 +0800

    feat: test

--- src/use/useToggle.ts
+++ src/use/useToggle.ts
@@ -1,10 +1,11 @@
 import { ref } from 'vue'
 import type { Ref } from 'vue'
-
+//kaiguan 开关
 export function useToggle(initState: boolean): [Ref<boolean>, () => void] {
   const state = ref(initState)
   const toggle = function () {
-    state.value = !state.value
+    const newValue = !state.value
+    state.value = newValue
   }
   return [state, toggle]
 }
